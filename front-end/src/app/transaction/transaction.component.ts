da83d09ff2d422731542b3e201aaf918b541f721
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-19 22:38:03 +0200

    adding function to get data from api

--- front-end/src/app/transaction/transaction.component.ts
+++ front-end/src/app/transaction/transaction.component.ts
@@ -5,24 +5,34 @@ import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
-  styleUrls: ['./transaction.component.css']
+  styleUrls: ['./transaction.component.css'],
 })
 export class TransactionComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute, private http:HttpClient) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
   page;
   per_page;
   seller_id;
 
-  ngOnInit(): void {
-    // Getting query parameters and saving them into variables
-    this.route.queryParams.subscribe((params: Params)=>{
-      this.page = params.page
-      this.per_page = params.per_page
-      this.seller_id = params.seller_id
-    })
-
-    // Calling the api to get the data
+  // Function to get data from api
+  getData(page, perPage, sellerId) {
+    return this.http.get(
+      `http://localhost:5000/transaction?page=${page}&per_page=${perPage}&seller_id=${sellerId}`
+    );
   }
 
+  ngOnInit(): void {
+    // Getting query parameters and saving them into variables
+    this.route.queryParams.subscribe((params: Params) => {
+      this.page = params.page;
+      this.per_page = params.per_page;
+      this.seller_id = params.seller_id;
+    });
+
+    // Calling the api to get the data
+    this.getData(this.page, this.per_page, this.seller_id).subscribe(
+      (recievedData) => {
+        console.log(recievedData);
+      }
+    );
+  }
 }
