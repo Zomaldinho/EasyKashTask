550b6b452e2dd69bc4ce4f983e3e1c7ff9e4d3d3
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-19 20:18:24 +0200

    add connection to DB

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const {Sequelize} = require('sequelize');
+
+// connecting to an AWS cloud database
+const db = new Sequelize('database_1', 'admin', 'test1234', {
+  host: 'database-1.c9r58b2hlfa8.eu-central-1.rds.amazonaws.com',
+  dialect: 'mysql',
+  port: 3306,
+});
 
 app = express();
 port = process.env.PORT || 5000;
