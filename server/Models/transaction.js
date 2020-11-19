25393da52369f8bedf0d89f975fe4a5519fccfd2
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-19 20:23:54 +0200

    defining a transaction table

---
+++ server/Models/transaction.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+
+// connecting to an AWS cloud MySql database
+const db = new Sequelize('database_1', 'admin', 'test1234', {
+  host: 'database-1.c9r58b2hlfa8.eu-central-1.rds.amazonaws.com',
+  dialect: 'mysql',
+  port: 3306,
+});
+
+// defining a transaction table inside the database
+const Transaction = db.define('transaction', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  fees: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = Transaction
