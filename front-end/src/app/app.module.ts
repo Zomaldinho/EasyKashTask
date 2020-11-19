929f9e65170eea996802661b4021e4781989cb58
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-19 22:27:26 +0200

    importing httpclient module to app

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TransactionComponent } from './transaction/transaction.component';
-import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes=[
   { path: 'transaction', component: TransactionComponent },
@@ -19,7 +20,8 @@ const appRoutes: Routes=[
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
