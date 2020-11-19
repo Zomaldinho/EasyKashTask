891c649aadc6f0edbd0b9a4bf87dde29907101b3
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-19 22:22:03 +0200

    adding listner to observe the query prams of transaction component routes

--- front-end/src/app/transaction/transaction.component.ts
+++ front-end/src/app/transaction/transaction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-transaction',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+  page;
+  per_page;
+  seller_id;
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params)=>{
+
+    })
   }
 
 }
