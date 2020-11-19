7904c786ca3d8a67037ba9e2fbad4a6c4457b843
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-19 20:20:14 +0200

    checking connection to DB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-// connecting to an AWS cloud database
+// connecting to an AWS cloud MySql database
 const db = new Sequelize('database_1', 'admin', 'test1234', {
   host: 'database-1.c9r58b2hlfa8.eu-central-1.rds.amazonaws.com',
   dialect: 'mysql',
   port: 3306,
 });
+
+//checking the connection is sucessfull
+db.authenticate()
+  .then(() => console.log('Successfully connected to DB'))
+  .catch((e) => console.log(e));
 
 app = express();
 port = process.env.PORT || 5000;
