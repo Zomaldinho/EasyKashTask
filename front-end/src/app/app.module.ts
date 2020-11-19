54c602fd5e775222c81234119f44949ecbe658d1
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-19 21:53:43 +0200

    adding home and transaction components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule
