644f58b248deb1b0d4b2ce0a8f21ecc452c25693
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-11-20 16:17:39 +0200

    adding seller table

---
+++ server/Models/seller.js
@@ -0,0 +1,22 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+// connecting to an AWS cloud MySql database
+const db = new Sequelize('database_1', 'admin', 'test1234', {
+  host: 'database-1.c9r58b2hlfa8.eu-central-1.rds.amazonaws.com',
+  dialect: 'mysql',
+  port: 3306,
+});
+
+// defining a transaction table inside the database
+const Seller = db.define('transaction', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  adress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Seller
